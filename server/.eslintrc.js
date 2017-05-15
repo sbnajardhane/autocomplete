843bf10f4659f0a95339ca475b0eee1268734594
shubham <sbnajardhane@gmail.com>
2017-05-15 18:54:43 +0530

    init the server and client folders on new release branch

---
+++ server/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "node": true,
+        "es6": true
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ]
+    }
+};
