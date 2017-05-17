1e3995763af2c2b81735b1dc7ba26f75ecc8d633
shubham <sbnajardhane@gmail.com>
2017-05-17 16:03:10 +0530

    set up the gulp to detect errors on save and to restart the server

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,15 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es6": true
+        "es6": true,
+        "node": true
     },
     "extends": "eslint:recommended",
     "parserOptions": {
         "sourceType": "module"
     },
     "rules": {
+        "no-console": "off",
         "indent": [
             "error",
             4
