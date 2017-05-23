e9efd60a3e5b756ca27e67e4f56622b2328e0cb0
Rohtash Singh <rohtash@webintensive.com>
2017-05-23 14:05:47 +0530

    added get suggestion functionlity in lookup

--- server/lookup_utils/main.js
+++ server/lookup_utils/main.js
@@ -1,94 +1,157 @@
-var timesort = require('timsort');
+/*
+ * Main file 
+    - to genrate data and index file
+    - to lookup for query
+ *
+*/
+
 var fs = require("fs");
+var sort = require("./sort.js");
 
-fs.readFile('in.txt', function(err, data) {
-    if (err) throw err;
-    var array = data.toString().split("\n");
-    array = mergeSort(array);
-    //   console.log(array);
-    generateLookUp(array);
-    //lookup("a", "english");
-});
-
-
+//10 digit base number for indexing 
+var baseNumber = "0000000000";
+var indexFile = "english.index";
+var lookup = "english.data";
 
 /*
- * Sorting logic :- Merge Sort
+ * generateLookUps
+ * @Algo
+    - read complete file
+    - do alphabetically sorting
+    - design lookfile and indexfile content
+    - write into files
  *
- */
-function mergeSort(arr) {
-    if (arr.length < 2) return arr;
+*/
 
-    var mid = Math.floor(arr.length / 2);
-    var subLeft = mergeSort(arr.slice(0, mid));
-    var subRight = mergeSort(arr.slice(mid));
+function generateLookupFiles(queriesFilePath) {
+    if (queriesFilePath == null || queriesFilePath == "") {
+        console.log("Invalid queriesFilePath");
+        return null;
+    }
+    readFile(queriesFilePath)
+        .then(sort)
+        .then(createLookUp)
+        .then(writeIntoFiles)
+        .catch((error) => {
+            console.log(error);
+            return null;
+        });
 
-    return merge(subLeft, subRight);
+
 }
-
-function merge(a, b) {
-    var result = [];
-    while (a.length > 0 && b.length > 0)
-        result.push(a[0] < b[0] ? a.shift() : b.shift());
-    return result.concat(a.length ? a : b);
-}
-
 
 /*
- * to design look up
+ * get Suggestion
+ * @Algo
+    - get dictonary (lookup and index files) name according to query
+    - analyse index file stat (number of entries)
+    - Binary search into data file
+    - return suggestions
  *
- */
+*/
 
-function generateLookUp(dataArray) {
+function getSuggestion(query) {
+    if (query == null || query == "") {
+        console.log("Invalid query");
+        return null;
+    }
 
-    var baseNumber = "0000000000";
-    var maxDepth = 8;
-    var index = baseNumber;
-    var indexFile = "english.index";
-    var lookup = "english.data";
-
-    var indexFileContent = '';
-    var dataFileContent = '';
-
-    dataArray.forEach(function(data, no) {
-
-
-        var valueLength;
-        var key = data;
-        var keylength = data.toString().length;
-        keylength = baseToNumber(keylength);
-
-
-        var value = '';
-        for (i = no + 1; i < dataArray.length; i++) {
-            if (dataArray[i].indexOf(data) !== -1) {
-                value += dataArray[i] + ',';
-            } else {
-                break;
-            }
-        }
-
-
-        //console.log(indexFileContent);
-        //console.log(index);
-
-        valueLength = baseToNumber(value.length);
-
-        //data file content
-        value = value.replace(/,\s*$/, "");
-        var content = `${keylength} ${key} ${valueLength} ${value}`;
-        console.log("Content length : " + content.length);
-
-        //indexing content
-
-        index = baseToNumber(index);
-        indexFileContent += index + '\n';
-        index = parseInt(index) + content.length;
-
-
-        dataFileContent += content + '\n';
-
+    return new Promise((resolve, reject) => {
+        getDictonary()
+            .then(getLookUp)
+            .then(([indexDes, dataDes, entries]) => {
+                var lower_one = 0;
+                var higher_one = entries;
+                var data = searchingIntoLookUp(query, indexDes, dataDes, lower_one, higher_one);
+                //console.log(data);
+                resolve(data);
+            }).catch((error) => {
+                console.log("Error : " + error);
+                reject(error);
+            });
     });
+}
+
+
+//to read queryfile
+function readFile(filepath) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(filepath, function(err, data) {
+            if (err) {
+                return reject(err);
+            }
+            var array = data.toString().split("\n");
+            resolve(array);
+
+        });
+    });
+}
+
+
+
+
+
+/*
+ * to create look up
+ *
+ */
+
+function createLookUp(dataArray) {
+
+    return new Promise((resolve) => {
+        var index = baseNumber;
+
+
+        var indexFileContent = "";
+        var dataFileContent = "";
+
+        dataArray.forEach(function(data, no) {
+
+
+            var valueLength;
+            var key = data;
+            var keylength = data.toString().length;
+            keylength = baseToNumber(keylength);
+
+
+            var value = "";
+            for (var i = no + 1; i < dataArray.length; i++) {
+                if (dataArray[i].indexOf(data) !== -1) {
+                    value += dataArray[i] + ",";
+                } else {
+                    break;
+                }
+            }
+
+
+            //console.log(indexFileContent);
+            //console.log(index);
+            value = value.trim();
+            valueLength = baseToNumber(value.length);
+
+            //data file content
+            if (value.length > 0) {
+                value = value.replace(/,\s*$/, "");
+                var content = `${keylength} ${key} ${valueLength} ${value}`;
+                console.log("Content : " + content);
+                console.log("Content length : " + content.length);
+
+                //indexing content
+
+                index = baseToNumber(index);
+                indexFileContent += index + "\n";
+                index = parseInt(index) + content.length + 1;
+
+
+                dataFileContent += content + "\n";
+            }
+
+        });
+        resolve([indexFileContent, dataFileContent]);
+    });
+}
+
+function writeIntoFiles([indexFileContent, dataFileContent]) {
 
     fs.writeFile(indexFile, indexFileContent, (err) => {
         if (err) {
@@ -104,7 +167,7 @@ function generateLookUp(dataArray) {
         } else {
             console.log("Lookup data file successfully created");
         }
-    })
+    });
     console.log(indexFileContent);
 }
 
@@ -116,54 +179,113 @@ function baseToNumber(number) {
     return base.slice(0, base.length - numberChar) + number;
 }
 
-function lookup(query, fileName) {
-    var indexFile = fileName + ".index";
-    var dataFile = fileName + ".data";
 
 
 
-    //fs.stats to get filesize 
-    const stats = fs.statSync("./english.index");
-    const fileSizeInBytes = stats.size
-    console.log(fileSizeInBytes);
-    const entries = parseInt((fileSizeInBytes) / 11);
-    console.log("Entries : " + entries);
+/********** Auto Suggestion **********/
 
-    //logics to build for binary
-    var lower_one = 0;
-    var higher_one = entries;
-    var middle_one = (entries + lower_one) / 2;
-    const indexDes = fs.openSync(indexFile, 'r');
-    const dataDes = fs.openSync(dataFile, 'r');
-    searchingIntoLookUp(indexDes, dataDes, lower_one, entries);
 
+function getDictonary() {
+    return new Promise((resolve) => {
+        //static filename for now 
+        var indexFile = "english.index";
+        var dataFile = "english.data";
+        resolve([indexFile, dataFile]);
+    });
+
+}
+
+function getLookUp([indexFile, dataFile]) {
+
+    return new Promise((resolve) => {
+
+        //fs.stats to get filesize 
+        const stats = fs.statSync("./english.index");
+        const fileSizeInBytes = stats.size;
+        //console.log(fileSizeInBytes);
+        const entries = parseInt((fileSizeInBytes) / 11);
+        //console.log("Entries : " + entries);
+
+        //file descriptor 
+        const indexDes = fs.openSync(indexFile, "r");
+        const dataDes = fs.openSync(dataFile, "r");
+        resolve([indexDes, dataDes, entries]);
+
+    });
 
 }
 
 
 /// Binary Searching 
-function searchingIntoLookUp(indexfileDes, datafileDes, lower_one, higher_one) {
-    if (lower_one < higher_one) {
+function searchingIntoLookUp(query, indexfileDes, datafileDes, lower_one, higher_one) {
+
+    if (lower_one <= higher_one) {
         var middle_one = Math.round((higher_one + lower_one) / 2);
-        console.log(higher_one);
-        console.log(middle_one)
+        //console.log(higher_one);
+        //console.log(middle_one);
         var seekToIndex = (middle_one * 11);
-        console.log(seekToIndex);
+        //console.log(seekToIndex);
 
 
-        const chunkSize = 11;
-        const fd = fs.openSync("english.data", 'r');
+        const chunkSize = 10;
         const buffer = new Buffer(chunkSize);
 
         // get middle_one position data from index file
         fs.readSync(indexfileDes, buffer, 0, chunkSize, seekToIndex);
         const seekToData = buffer.toString();
-        console.log(seekToData);
+        // console.log("seek to data value : " + seekToData);
 
         //get Data from data file at seekToData position
         fs.readSync(datafileDes, buffer, 0, chunkSize, parseInt(seekToData));
-        console.log(buffer.toString());
+        const keylength = parseInt(buffer.toString());
 
 
+        //get key value form datafile
+        //console.log("Key length : " + keylength);
+        var seekToKey = parseInt(seekToData) + parseInt(chunkSize) + 1;
+        const bufferb = new Buffer(50);
+        fs.readSync(datafileDes, bufferb, 0, parseInt(keylength), seekToKey);
+        var data = bufferb.slice(0, parseInt(keylength)).toString();
+
+        //console.log("Key : " + data)
+
+
+        if (data.trim() == query.trim()) {
+            console.log("matched ... !!!");
+            const seekToValueLength = seekToKey + keylength + 1;
+
+            //get value length from data file at seekToValueLength position
+            fs.readSync(datafileDes, buffer, 0, chunkSize, parseInt(seekToValueLength));
+            const valueLength = parseInt(buffer.toString());
+
+
+            //get key value form datafile
+            //console.log("Key length : " + keylength);
+            const seekToValue = parseInt(seekToValueLength) + parseInt(chunkSize) + 1;
+            const bufferb = new Buffer(valueLength);
+            fs.readSync(datafileDes, bufferb, 0, parseInt(valueLength), seekToValue);
+            var suggestions = bufferb.toString();
+            // console.log(suggestions);
+            return suggestions;
+
+        } else if (data > query) {
+            higher_one = middle_one - 1;
+            return searchingIntoLookUp(query, indexfileDes, datafileDes, lower_one, higher_one);
+        } else if (data < query) {
+            lower_one = middle_one + 1;
+            return searchingIntoLookUp(query, indexfileDes, datafileDes, lower_one, higher_one);
+        }
+
+    } else {
+        console.log("Not founded");
+        return "";
     }
 }
+
+/*
+ * MOdule Exports function 
+ *
+ */
+
+module.exports.getSuggestion = getSuggestion;
+module.exports.generateLookupFiles = generateLookupFiles;
