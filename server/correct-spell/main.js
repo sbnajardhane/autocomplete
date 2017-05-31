69d8712e7bc71472ddc1c8af3649fd07751832d7
Mayuri Rathod <mayuri@webintensive.com>
2017-05-31 12:16:34 +0530

    Remove the readline approach and keep only express

--- server/correct-spell/main.js
+++ server/correct-spell/main.js
@@ -2,7 +2,6 @@
 The main file to create dictionary or to get suggestion
  */
 
-const readLine = require("readline");
 const lookUp = require("./lookup");
 const express = require("express");
 
@@ -12,53 +11,21 @@ var options = {
     debug: true
 };
 var lookup = new lookUp(options);
-var api = true;
 
-if(api) {
-    var app = express();
-    const port = 3101;
-    // get request to the autocomplete suggestions
-    app.get("/api/autocomplete/:_language/:_query", (req, res) => {
-        let query = req.params._query;
-        let language = req.params._language;
-        console.log("query", query);
-        lookup.correct(query, language).then((suggestions) => {
-            console.log("suggestions", suggestions);
-            res.json(suggestions);
-        }).catch((err) => {
-            console.log("error", err);
-            res.json({ error: err });
-        });
+var app = express();
+const port = 3101;
+// get request to the autocomplete suggestions
+app.get("/api/autocomplete/:_language/:_query", (req, res) => {
+    let query = req.params._query;
+    let language = req.params._language;
+    console.log("query", query);
+    lookup.correct(query, language).then((suggestions) => {
+        console.log("suggestions", suggestions);
+        res.json(suggestions);
+    }).catch((err) => {
+        console.log("error", err);
+        res.json({ error: err });
     });
-    // Listen on port
-    app.listen(port);
-
-} else {
-    var rl = readLine.createInterface({
-        input: process.stdin,
-        output: process.stdout,
-        terminal: false
-    });
-
-    rl.on("line", function(word) {
-        if(word === ".exit") {
-            process.exit();
-        }
-        if (typeof word !== undefined && word !== "") {
-            var suggestions = lookup.correct(word.trim(), "");
-            console.log("suggestions", suggestions);
-            suggestions = suggestions.suggestions;
-            for (var key in suggestions) {
-                var suggestion = suggestions[key];
-                if (suggestion.distance === 0) {
-                    console.log("spelling is correct");
-                    break;
-                }
-                else {
-                    console.log(suggestion.term + " " + suggestion.distance + " " + suggestion.count, suggestion);
-                }
-            }
-            console.log(suggestions.length + "  suggestions");
-        }
-    });
-}
+});
+// Listen on port
+app.listen(port);
